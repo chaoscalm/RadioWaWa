1b0df873ecdf77590e21962a27eba4147463f5af
chaoscalm <7214961+chaoscalm@users.noreply.github.com>
2025-02-18 13:47:10 +0100

    Update WAWA.java

--- app/src/main/java/com/radio/wawa/WAWA.java
+++ app/src/main/java/com/radio/wawa/WAWA.java
@@ -9,21 +9,25 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 public class WAWA implements IXposedHookLoadPackage {
+
+    @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        // Check if the loaded package is the target FM radio package
         if (!lpparam.packageName.equals("com.caf.fmradio"))
             return;
 
         // XposedBridge.log("[WAWA] started");
 
-        // https://github.com/LineageOS/android_vendor_qcom_opensource_fm-commonsys/blob/lineage-21.0/fmapp2/src/com/caf/fmradio/FMRadioService.java
-        // https://github.com/LineageOS/android_vendor_qcom_opensource_fm-commonsys
-       findAndHookMethod("com.caf.fmradio.FMRadioService", lpparam.classLoader, "isAntennaAvailable", new XC_MethodReplacement() {
+        // Hook the isAntennaAvailable method in FMRadioService
+        findAndHookMethod("com.caf.fmradio.FMRadioService", lpparam.classLoader, "isAntennaAvailable", new XC_MethodReplacement() {
             @Override
             protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam methodHookParam) throws Throwable {
                 // XposedBridge.log("[WAWA] hooked FMRadioService.isAntennaAvailable");
                 return true;
             }
         });
+
+        // Hook the isAntennaAvailable method in FMRadio
         findAndHookMethod("com.caf.fmradio.FMRadio", lpparam.classLoader, "isAntennaAvailable", new XC_MethodReplacement() {
             @Override
             protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam methodHookParam) throws Throwable {
